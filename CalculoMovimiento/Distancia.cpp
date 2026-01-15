78dde5a40b9e5e50f50f52b386c384e039fb919d
Laptop <Laptop@DESKTOP-KN5O1DM>
2026-01-15 18:45:57 -0500

    Agrega comentarios a la clase Distancia

--- CalculoMovimiento/Distancia.cpp
+++ CalculoMovimiento/Distancia.cpp
@@ -1,3 +1,5 @@
+// Calcula la distancia recorrida usando la fórmula:
+// distancia = velocidad * tiempo
 #include "Distancia.h"
 
 double Distancia::calcularDistancia(double velocidad, double tiempo) {
